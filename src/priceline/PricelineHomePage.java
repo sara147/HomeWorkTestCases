41e132d478353ad2625bf17eb06ff2eb26ba10b8
sara147 <saraibel06@gmail.com>
2021-03-06 14:28:06 -0500

    pricelineHomePage

--- src/priceline/PricelineHomePage.java
+++ src/priceline/PricelineHomePage.java
@@ -1,6 +1,19 @@
 package priceline;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class PricelineHomePage {
+    public static void main(String[] args) {
+         String pricelineUrl = "https://www.priceline.com/";
+         String ChromeDriverPath = "windows/chromedriver.exe";
+         System.setProperty("webdriver.chrome.driver",ChromeDriverPath);
+         WebDriver driver =new ChromeDriver();
+         driver.get(pricelineUrl);
+         driver.manage().window().maximize();
+         driver.manage().deleteAllCookies();
+         driver.get("https://www.priceline.com/");
 
-
+    }
 }
